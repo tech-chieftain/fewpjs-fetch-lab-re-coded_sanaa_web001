257738c31570d8c1d9b50a59c77cdaed0ad79113
Salah Al-Dhaferi <binaryminded@gmail.com>
2020-09-03 10:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function fetchBooks() {
+// function fetchBooks() {
 
-}
+// }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main')
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = book.name
+//     main.appendChild(h2)
+//   })
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
